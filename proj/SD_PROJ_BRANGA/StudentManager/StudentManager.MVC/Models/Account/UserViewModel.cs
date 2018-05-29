a43a113ee3008077bcf5afd8e28847364afb39ae
ANDREIBRANG72A0\andrei <andrei_branga@icloud.com>
2018-05-29 21:52:43 +0300

    last proj commit

--- proj/SD_PROJ_BRANGA/StudentManager/StudentManager.MVC/Models/Account/UserViewModel.cs
+++ proj/SD_PROJ_BRANGA/StudentManager/StudentManager.MVC/Models/Account/UserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,12 @@ namespace Restaurant.MVC.Models.Account
 {
     public class UserViewModel
     {
+        [Key]
         public int UserId { get; set; }
         public string Id { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        [EmailAddress]
         public string UserName { get; set; }
         public Nullable<int> HotelChainId { get; set; }
 
